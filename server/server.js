2177fc632ccfde5c7ff46ac7a78f137e483565c0
Anish Sana <anishsana91@gmail.com>
2017-07-03 12:48:08 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {
+  ObjectID
+} = require('mongodb');
 
 var {
   mongoose
@@ -37,6 +40,19 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((e) => console.log(e));
 });
 
 app.listen(3000, () => {
