4f31b30fabdf6935ff6930d75468507b9b9450e6
Anish Sana <anishsana91@gmail.com>
2017-07-04 12:09:04 -0400

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -49,10 +49,14 @@ app.get('/todos/:id', (req, res) => {
   };
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
-    res.send({todo});
-  }).catch((e) => console.log(e));
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
